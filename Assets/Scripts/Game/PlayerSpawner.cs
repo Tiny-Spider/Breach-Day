732e6a24f95cb21d3c017dfeaf6c8f6cbaf6921f
Mark Alberts <mark1996a@hotmail.com>
2014-12-12 16:01:09 +0100

    Added multiplayer player spawning, added player network sync

--- Assets/Scripts/Game/PlayerSpawner.cs
+++ Assets/Scripts/Game/PlayerSpawner.cs
@@ -1,11 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerSpawner : MonoBehaviour {
-    // TODO: fix
+    public Transform spawnA;
+    public Transform spawnB;
+    
 	void Start () {
-        if (NetworkManager.instance.GetMyInfo().team == "teamA") {
-            //Network.Instantiate()
+        if (Network.isServer) {
+            NetworkManager networkManager = NetworkManager.instance;
+
+            foreach (KeyValuePair<NetworkPlayer, PlayerInfo> entry in networkManager.connectedPlayers) {
+                bool teamA = entry.Value.team.Equals("teamA");
+                Player player = Network.Instantiate(teamA ? PrefabManager.instance.teamA : PrefabManager.instance.teamB, teamA ? spawnA.position : spawnB.position, teamA ? spawnA.rotation : spawnB.rotation, 0) as Player;
+                player.networkView.RPC("SetPlayer", entry.Key);
+            }
         }
 	}
 }
